bd11e87cb7c0d9e9f4833c75067b50c45aab55d7
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-04-27 23:36:26 +0200

    🚸 Rework CLI args

--- restapiboys/cli/index.py
+++ restapiboys/cli/index.py
@@ -4,21 +4,22 @@ Usage:
   restapiboys [--help] [options] <command>
 
 Options:
-  -v --log-level=LEVEL  Show log messages of at most this level. [default: info]
+  -L --log=LEVEL  Show log messages of at most this level. [default: info]
                         Possible values:
                             debug    - Shows all messages, including debug information
                             info     - Informative messages
                             warning  - Message that informs of potentially erroneous
                             error    - Error messages
                             critical - Show only critical messages
-  -q --quiet            Equivalent of --log-level=critical
+  -q --quiet            Equivalent of --log=critical
+  -v --verbose          Equivalent of --log=debug
   -c --config=FILEPATH  Specify the configuraiton file path [default: config.yaml]
 
 Command 'start' options:
   -p --port=PORT               Port number [default: 8888]
   --address=IP_ADDRESS         Listen on this IP address [default: 127.0.0.1]
   --run-in-background          Run the webserver as a background process
-  --watch                      Restart webserver when code changes
+  -w --watch                   Restart webserver when code changes
   --workers=INTEGER|'auto'     Number of gunicorn workers to boot [default: auto]
 """
 from enum import Enum
@@ -26,6 +27,7 @@ import os
 from logging import getLogger
 from typing import *
 from importlib import import_module
+from restapiboys.cli import start
 import docopt
 
 
@@ -40,22 +42,20 @@ def entry_point() -> None:
     args = docopt.docopt(__doc__)
     if args['--quiet']:
         os.environ['log-level'] = 'CRITICAL'
+    if args['--verbose']:
+        os.environ['log-level'] = 'DEBUG'
     else:
-        os.environ['log-level'] = args.get('--log-level', 'INFO').upper()
+        os.environ['log-level'] = args.get('--log', 'INFO').upper()
     logger.setLevel(os.environ.get('log-level', 'INFO'))
     subcommand = args["<command>"]
     dispatch_subcommand(subcommand, args)
 
 
 def dispatch_subcommand(subcommand_name: str, args: Dict[str, Any]):
-    try:
-        subcommand = import_module(f"restapiboys.cli.{subcommand_name}")
-    except ModuleNotFoundError:
-        raise CommandNotFoundError(f"Command {subcommand_name!r} not found.")
+    if subcommand_name == 'start':
+        start.run(args)
+        return None
 
-    try:
-        subcommand.run(args)
-    except AttributeError:
-        raise NotImplementedError(
-            f"Command {subcommand_name!r} not implemented. (does not define a 'run' function)"
-        )
+    raise NotImplementedError(
+        f"Command {subcommand_name!r} not implemented. (does not define a 'run' function)"
+    )
