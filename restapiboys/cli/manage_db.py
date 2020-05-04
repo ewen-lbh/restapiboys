0cda9b73f94bafd456d98a4923502e2064388358
ewen-lbh <ewen.lebihan7@gmail.com>
2020-05-04 12:01:10 +0200

    🚸 cli/manage-db: Better UI for database creation

--- restapiboys/cli/manage_db.py
+++ restapiboys/cli/manage_db.py
@@ -1,5 +1,5 @@
 from restapiboys.endpoints import get_endpoints
-from restapiboys.log import info
+from restapiboys.log import info, warn, error
 from restapiboys.database import COUCHDB_PORT, create_database, database_exists
 from typing import *
 import webbrowser
@@ -11,7 +11,7 @@ def run(args: Dict[str, Any]) -> None:
     info('Opening {} in your webbrowser...', url)
     webbrowser.open(url)
 
-def create_databases() -> Dict[str, bool]:
+def create_databases() -> None:
     """
     Creates all missing databases, and returns a dict with the format:
     
@@ -24,13 +24,17 @@ def create_databases() -> Dict[str, bool]:
     """
     
     databases_names = [ r.identifier for r in get_endpoints() ]
-    created_databases = {}
+    missing_databases = []
 
     for name in databases_names:
         if not database_exists(name):
-            info('Database {} does not exist, creating it...', name)
-            created = create_database(name)
-            created_databases[name] = created
-
-    return created_databases
+            missing_databases.append(name)
+    
+    if missing_databases:
+        warn('Missing {} databases', len(missing_databases))
+        for db_name in missing_databases:
+            info('\t- Creating database {}', db_name)
+            created = create_database(db_name)
+            if not created:
+                error('\t  Could not create {}', db_name)
             
